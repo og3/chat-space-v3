e194e0841786bf3c8b76d201fcbdbb2874b3c6ea
noriyuki togashi <n-togashi@xxxx.local>
2016-12-01 12:06:14 +0900

    ・delete message.yml ・change action name  chat to index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,4 @@
 class MessagesController < ApplicationController
-  def chat
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root "messages#chat"
+  root "messages#index"
 end
