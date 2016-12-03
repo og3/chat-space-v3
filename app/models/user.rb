8890e54ea19076dfea2845c5c38d8c3d310330b8
noriyuki togashi <n-togashi@xxxx.local>
2016-12-03 12:03:38 +0900

    ・add validates to name email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
-  
+
 end
