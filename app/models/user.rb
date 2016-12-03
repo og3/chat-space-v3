a0d76e14a51c8949024f350b090a2857ac90da34
noriyuki togashi <n-togashi@xxxx.local>
2016-12-03 12:13:17 +0900

    ・delete email columns validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
