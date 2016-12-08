cd303ee9c0c0310f55363ff97484a621cf133759
noriyuki togashi <n-togashi@xxxx.local>
2016-12-08 14:39:22 +0900

    Correction　associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,10 +2,10 @@ class Group < ApplicationRecord
   
   validates :name, presence: true
 
-  has_many :users, through: :group_users  
+  has_many :users, :through => :group_users  
   has_many :messages  
   has_many :group_users  
 
   accepts_nested_attributes_for :users
-  
+
 end
