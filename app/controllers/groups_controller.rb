d3a3800b6989d8d1732f1fdf67f9534cea175f10
noriyuki togashi <n-togashi@xxxx.local>
2016-12-09 14:09:17 +0900

    add create group action (new & create action)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,22 @@
+
 class GroupsController < ApplicationController
 
   def new
-
+    @group = Group.new
   end
 
+  def create
+    @group = Group.create(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループが作成されました'
+    else
+      flash.now[:alert] = "グループ名を入力してください"
+      render :action => :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { user_ids:[] })
+  end
 end
