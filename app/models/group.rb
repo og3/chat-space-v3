7569ca3211167bcfb2dd5b52f9bc10d0e808f07e
noriyuki togashi <n-togashi@xxxx.local>
2016-12-10 12:00:04 +0900

    delete =>

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   
   validates :name, presence: true
 
-  has_many :users, :through => :group_users  
+  has_many :users, through: :group_users  
   has_many :messages  
   has_many :group_users  
 
