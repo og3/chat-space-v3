0efcbf9acd0a34175baefedb763f35cf0a169f98
noriyuki togashi <n-togashi@xxxx.local>
2016-12-15 16:11:07 +0900

    add index show edit update action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < ApplicationController
 
+  def index
+    @groups = current_user.groups
+  end
+
   def new
     @group = Group.new
   end
@@ -12,6 +16,25 @@ class GroupsController < ApplicationController
       flash.now[:alert] = "グループ名を入力してください"
       render :new
     end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_path(@group), flash: {notice: 'グループ情報が変更されました。'}
+    else
+      render 'edit'
+      flash[:alert] = 'グループ情報の変更に失敗しました'
+    end
+  end
+
+  def show
+    @groups = current_user.groups
+    @group = Group.find(params[:id])
   end
 
   private
