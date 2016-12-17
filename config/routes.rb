7965fba6419c0aaed3359da808a1a2f2c9c96368
noriyuki togashi <n-togashi@xxxx.local>
2016-12-17 20:54:19 +0900

    nest messages to groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'groups#index'
   devise_for :users
-  resources :messages
   resources :groups, only: [:new, :create, :update, :edit, :show, :index]
+    resources :messages
   
 end
