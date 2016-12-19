de394be6c8220976b1e924ec9c2adb7b6a132c4c
noriyuki togashi <n-togashi@xxxx.local>
2016-12-19 18:59:04 +0900

    add create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,18 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-  def index
+
+  def create
+    @message = Message.new(create_params) #入力された値を保存する
+    @group = params[:group_id]  #リダイレクトをするために、現在のページのgroup_idを取得する
+    if @message.save!
+      redirect_to group_path(@group)
+    else redirect_to :back, alert: 'メッセージを入力してください'
+    end
+
+  end
+
+  private
+  def create_params
+    params.require(:message).permit(:message, :user_id, :group_id).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
