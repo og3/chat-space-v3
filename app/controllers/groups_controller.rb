80a488ee389fd11dc053824cb42f53ce158514cf
noriyuki togashi <n-togashi@xxxx.local>
2016-12-19 22:13:33 +0900

    delete show action and change path

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-  before_action :set_group, only:[:show, :edit, :update]
+  before_action :set_group, only:[:edit, :update]
 
   def index
     @groups = current_user.groups
@@ -13,7 +13,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to group_path(@group), notice: 'グループが作成されました'
+      redirect_to group_messages_path(@group), notice: 'グループが作成されました'
     else
       flash.now[:alert] = "グループ名を入力してください"
       render :new
@@ -25,18 +25,11 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_path(@group), notice: 'グループ情報が変更されました。'
+      redirect_to group_messages_path(@group), notice: 'グループ情報が変更されました。'
     else
       render :edit
       flash[:alert] = 'グループ情報の変更に失敗しました'
     end
-  end
-
-  def show
-    @group = Group.find(params[:id]) #グループのページにアクセスした時に、そのグループの番号を受け取る
-    @groups = current_user.groups #ログイン中のユーザーが属するグループの情報を持ってくる
-    @message = Message.new #form_forに入力される値を受け取るための箱を用意する
-    @messages = @group.messages #上で受け取ったグループの番号に関連したmessageテーブルの値を取得する
   end
 
   private
