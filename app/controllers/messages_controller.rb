67b8bd75f92920218f6ed2edb0f80c34f927ebd6
noriyuki togashi <n-togashi@xxxx.local>
2016-12-19 20:03:58 +0900

    collection message controller save! => save add space delete permit group_id user_id

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,16 +3,17 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(create_params) #入力された値を保存する
-    @group = params[:group_id]  #リダイレクトをするために、現在のページのgroup_idを取得する
-    if @message.save!
-      redirect_to group_path(@group)
+    @group_id = params[:group_id]  #リダイレクトをするために、現在のページのgroup_idを取得する
+    if @message.save
+      redirect_to group_path(@group_id)
     else redirect_to :back, alert: 'メッセージを入力してください'
+
     end
 
   end
 
   private
   def create_params
-    params.require(:message).permit(:message, :user_id, :group_id).merge(user_id: current_user.id, group_id: params[:group_id])
+    params.require(:message).permit(:message).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
