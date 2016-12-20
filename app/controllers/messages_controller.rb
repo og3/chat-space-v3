72318e9db4317d488d12dd5383fcc9e5721de9b8
noriyuki togashi <n-togashi@xxxx.local>
2016-12-20 20:18:27 +0900

    collection redirect in create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,16 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group, only:[:index]
 
   def index
-    @group = Group.find(params[:group_id]) #グループのページにアクセスした時に、そのグループの番号を受け取る
     @messages = @group.messages #上で受け取ったグループの番号に関連したmessageテーブルの値を取得する
     @message = Message.new #form_forに入力される値を受け取るための箱を用意する
   end
 
   def create #ここでの変数はviewでは使うことがないので、ローカル変数にする
     message = Message.new(create_params) #入力された値を保存する
-    group_id = params[:group_id]  #リダイレクトをするために、現在のページのgroup_idを取得する
     if message.save
-      redirect_to group_messages_path(group_id)
+      redirect_to group_messages_path(params[:group_id]) #アドレスに入力されているgroup_idのページにリダイレクトする
     else 
       redirect_to :back, alert: 'メッセージを入力してください'
     end
@@ -21,5 +20,9 @@ class MessagesController < ApplicationController
   private
   def create_params
     params.require(:message).permit(:message).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id]) #グループのページにアクセスした時に、そのグループの番号を受け取る
   end
 end
