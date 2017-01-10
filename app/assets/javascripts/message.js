8cfbfc36be64f4a6f08e3ac4c054efae962332fc
noriyuki togashi <n-togashi@xxxx.local>
2017-01-10 16:39:57 +0900

    create ajax for send message

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,47 @@
+$(function() {
+  function buildHTML(adddata) {
+// 新規に入力された値を入れるhtmlを新規に作る
+    var html =
+      '<li class="chat__main__body__chat__set">'              +
+        '<p class="chat__main__body__chat__set__name">'       +
+          adddata.name                                        +
+        '</p>'                                                +
+        '<p class="chat__main__body__chat__set__time_stamp">' +
+          adddata.datetime                                    +
+        '</p>'                                                +
+        '<p class="chat__main__body__chat__set__message">'    +
+          adddata.message                                     +
+        '</p>'                                                +
+      '</li>'
+    return html;
+  }
+
+//sendボタンがclickされた時に以下のイベントが行われるようにする。（メッセージ送信後にsendボタンにdisabledがかからないようにするため）
+  $('.chat__main__footer__send_button').click('submit', function(e) {
+//フォームが送信された時に、デフォルトだとフォームを送信するための通信がされてしまうので、preventDefault()を使用してデフォルトのイベントを止める
+    e.preventDefault(); 
+    var textField = $('.js-form__text-area'); //class js-form__text-fieldを代入
+    var message = textField.val(); //js-form__text-fieldのフォームに入力された値を取得し、messageに代入
+    $.ajax({
+      url: window.location.href,
+      type: 'POST',
+      data: {
+        message: {
+          message: message //二重ハッシュ構造 messageハッシュの中にmessageキーとmessageバリューが入っている。messageの名前にしてるのはコントローラーのストロングパラメータに対応させるため。
+        }
+      },
+      dataType: 'json' //データをjson形式でリクエストする
+    })
+    //↓フォームの送信に成功した場合の処理
+    .done(function(data) {
+      console.log(data);
+      var html = buildHTML(data);
+      $('ul.chat__main__body__chat').append(html); //ul.chat__main__body__chatを選択し、上の変数htmlを挿入している
+      textField.val('');
+    })
+    //↓フォームの送信に失敗した場合の処理
+    .fail(function() {
+      alert('error');
+    });
+  });
+});
