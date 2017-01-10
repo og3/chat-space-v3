733c3e9b14be358cffaf2465eabc61c163bc031f
noriyuki togashi <n-togashi@xxxx.local>
2017-01-10 20:03:47 +0900

    change class name

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,7 +20,7 @@ $(function() {
   $('.chat__main__footer__send_button').click('submit', function(e) {
 //フォームが送信された時に、デフォルトだとフォームを送信するための通信がされてしまうので、preventDefault()を使用してデフォルトのイベントを止める
     e.preventDefault(); 
-    var $textField = $('.message-form__text-area'); //class js-form__text-fieldを代入
+    var $textField = $('.js-message-form__text-area'); //class js-form__text-fieldを代入
     var message = $textField.val(); //js-form__text-fieldのフォームに入力された値を取得し、messageに代入
     $.ajax({
       url: window.location.href,
