c82cacd6a8e7008b05688dae3f41736ce2da0eb8
noriyuki togashi <n-togashi@xxxx.local>
2017-01-16 17:44:19 +0900

    add index action for get users data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    # users_tableから曖昧検索をして変数usersに入れる やり方は、where("カラム名 like '%検索テキスト%'")
+    @users = User.where('name like ?', "%#{params[:name]}%")
+    # json形式で値を返す
+    render json: @users
+  end
 end
