03f3cb2a99d1309e11952dfca199ad3d26604c3c
noriyuki togashi <n-togashi@xxxx.local>
2017-01-20 14:35:52 +0900

    add async send image function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,11 @@
 $(function() {
   function buildHTML(adddata) {
+    // 引数adddataにimageが入っていたら、表示するhtmlを追加する
+    if(adddata.image){
+      var addImage = '<br><img src="' + adddata.image + '">';
+    }else{
+      var addImage = '';
+    }
 // 新規に入力された値を入れるhtmlを新規に作る。記法はHTMLで書く。
     var html =
       '<li class="chat__main__body__chat__set">'              +
@@ -11,26 +17,26 @@ $(function() {
         '</p>'                                                +
         '<p class="chat__main__body__chat__set__message">'    +
           adddata.message                                     +
+          addImage                                            +
         '</p>'                                                +
       '</li>'
     return html;
   }
 
 //sendボタンがclickされた時に以下のイベントが行われるようにする。（メッセージ送信後にsendボタンにdisabledがかからないようにするため）
-  $('.chat__main__footer__send_button').click('submit', function(e) {
+  $('.send-button').click('submit', function(e) {
 //フォームが送信された時に、デフォルトだとフォームを送信するための通信がされてしまうので、preventDefault()を使用してデフォルトのイベントを止める
     e.preventDefault(); 
-    var $textField = $('.js-message-form__text-area'); //class js-form__text-fieldを代入
-    var message = $textField.val(); //js-form__text-fieldのフォームに入力された値を取得し、messageに代入
+    // $('#new_message')はform_forで自動生成されたidで、すべての入力欄の親要素
+    var form = $('#new_message').get(0);
+    var formData = new FormData(form);
     $.ajax({
-      url: window.location.href,
+      url: './messages.json',
       type: 'POST',
-      data: {
-        message: {
-          message: message //二重ハッシュ構造 messageハッシュの中にmessageキーとmessageバリューが入っている。messageの名前にしてるのはコントローラーのストロングパラメータに対応させるため。
-        }
-      },
-      dataType: 'json' //データをjson形式でリクエストする
+      data: formData,
+      dataType: 'json', //データをjson形式でリクエストする
+      processData: false,
+      contentType: false,
     })
     //↓フォームの送信に成功した場合の処理
     .done(function(data) {
@@ -38,11 +44,17 @@ $(function() {
       //ul.chat__main__body__chatを選択し、上の変数htmlを挿入している
       $('ul.chat__main__body__chat').append(html); 
       // textfieldを空にする
-      $textField.val('');
+      form.reset();
     })
     //↓フォームの送信に失敗した場合の処理
     .fail(function() {
       alert('error');
+    });
+
+    // file_fieldがchangeしたら発動。form_forをsubmitさせる。
+    $('#message-image').on('change', function(){
+      $(this).parents('#new_message').submit();
+      form.reset();
     });
   });
 });
