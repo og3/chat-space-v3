adcbab14cc8259c2a83f407d815140f640f3f663
noriyuki togashi <n-togashi@xxxx.local>
2017-01-24 21:20:14 +0900

    add region code in adress

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,19 @@
 CarrierWave.configure do |config|
   # config.storage = :fog
-  config.fog_provider = 'fog/aws'
+  # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeaset-1'
+    region: 'ap-northeast-1'
+    # endpoint: 'chatspaceimages.s3.amazonaws.com'
   }
 
-  case Rails.env
-    when 'production'
-      config.fog_directory = 'chatspaceimages'
-      config.asset_host = 'https://s3.amazonaws.com/chatspaceimages'
+    config.fog_directory = 'chatspaceimages'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspaceimages'
 
-    when 'development'
-      config.fog_directory = 'chatspaceimages'
-      config.asset_host = 'https://s3.amazonaws.com/chatspaceimages'
-
-    when 'test'
-      config.storage = :file
+  if (Rails.env == 'development')
+    config.fog_directory = 'chatspaceimages'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspaceimages'
   end
 end
